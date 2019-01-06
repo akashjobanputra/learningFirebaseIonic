9038f9fb2c36bb21ca6cd44a88334308db598388
Akash Jobanputra <akash.jobanputra@hotmail.com>
2019-01-06 23:12:30 +0530

    added sample firebase credential file

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxx",
+    authDomain: "xxxxxxxxxxxxxxxxxxxxxxxxx.firebaseapp.com",
+    databaseURL: "https://xxxxxxxxxxxxxxxxxxxxxxxxx.firebaseio.com",
+    projectId: "xxxxxxxxxxxxxxxxxxxxxxxxx",
+    storageBucket: "xxxxxxxxxxxxxxxxxxxxxxxxx.appspot.com",
+    messagingSenderId: "xxxxxxxxxxxxxxxxxxxxxxxxx"
+};
